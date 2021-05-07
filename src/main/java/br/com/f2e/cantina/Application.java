7865cb3af9399b9b75757b3d2abc07717ea22686
Franklin Barreto <franklin-barreto@hotmail.com>
2021-05-07 15:30:16 -0300

    first commit

---
+++ src/main/java/br/com/f2e/cantina/Application.java
@@ -0,0 +1,13 @@
+package br.com.f2e.cantina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
