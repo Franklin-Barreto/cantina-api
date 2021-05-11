f30222f27bd5fb5020437232bddf2606daebaa3b
Franklin Barreto <franklin-barreto@hotmail.com>
2021-05-11 17:51:34 -0300

    add initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO address VALUES (null,'Av Osvaldo do Vale Cordeiro','1360','São Paulo','SP');
+INSERT INTO address VALUES (null,'Rua Pedro Barbosa','194','São Paulo','SP');
+INSERT INTO customer VALUES (null,'123456789','Franklin Barreto','11975215595',2);
+INSERT INTO customer VALUES (null,'987654321','Anselmo Alves','118889999',1);
+
